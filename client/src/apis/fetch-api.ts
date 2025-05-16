b485a8a3d0e666e5d407f100427c4ae244c738be
sahajb <sahajb@minion25.machine>
2025-05-16 15:54:18 +0530

    Fetch api seperate client created

---
+++ client/src/apis/fetch-api.ts
@@ -0,0 +1,47 @@
+import qs from "qs";
+import { getStrapiURL } from "./api-helper";
+import { IApiResponse } from "./dtos/shared-component.type";
+
+export class FetchAPIClient {
+  private baseHeaders = {
+    "Content-Type": "application/json",
+  };
+
+  private basePath: string;
+
+  constructor(path?: string) {
+    this.basePath = path ? path : "/api";
+  }
+
+  async get<T>(path: string, urlParamsObject = {}, options: RequestInit = {}) {
+    try {
+      const mergedOptions = {
+        next: { revalidate: 60 },
+        headers: {
+          ...this.baseHeaders,
+          ...(options.headers || {}),
+        },
+        ...options,
+      };
+
+      const queryString = qs.stringify(urlParamsObject);
+
+      const requestUrl = `${getStrapiURL(`${this.basePath}${path}`)}${
+        queryString ? `?${queryString}` : ""
+      }`;
+
+      const response = await fetch(requestUrl, mergedOptions);
+
+      if (!response.ok) {
+        throw new Error(`API request failed with status ${response.status}`);
+      }
+
+      const data: IApiResponse<T> = await response.json();
+      return data;
+    } catch (error) {
+      throw new Error(
+        `Please check if your server is running and you set all the required tokens.`
+      );
+    }
+  }
+}
